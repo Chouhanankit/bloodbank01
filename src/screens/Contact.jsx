76f286ba19c47659bf83af09b9a1b33083617475
Ankit Chouhan <99067327+Chouhanankit@users.noreply.github.com>
2024-12-29 01:43:06 +0530

    6th Commit

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/16/solid'
 import { Field, Label, Switch } from '@headlessui/react'
 
-const ConatctList = () => {
+const ContactList = () => {
     const [agreed, setAgreed] = useState(false)
     return (
         <div className="isolate bg-red-500 px-6 py-24 sm:py-32 lg:px-8">
